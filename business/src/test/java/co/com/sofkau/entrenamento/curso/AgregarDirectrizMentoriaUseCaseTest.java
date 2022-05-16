b440fbd3fb3b1ecf1aeb7be04315625d2bd02692
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-16 16:13:00 -0500

    Creación de la prueba para AgregarDirectrizMentoria.

---
+++ business/src/test/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCaseTest.java
@@ -0,0 +1,71 @@
+package co.com.sofkau.entrenamento.curso;
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarMentoria;
+import co.com.sofkau.entrenamiento.curso.events.DirectrizAgregadaAMentoria;
+import co.com.sofkau.entrenamiento.curso.events.MentoriaCreada;
+import co.com.sofkau.entrenamiento.curso.values.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AgregarDirectrizMentoriaUseCaseTest {
+
+    @InjectMocks
+    private AgregarDirectrizMentoriaUseCase useCase;
+
+    @Mock
+    private DomainEventRepository repository;
+
+    @Test
+    void agregarDirectrizUnaMentoriaHappyPass(){
+        //arrange
+        MentoriaId mentoriaId = new MentoriaId("xxxx");
+        Directiz directiz = new Directiz("Curso DDD");
+        var command = new AgregarDirectrizMentoria(mentoriaId, directiz);
+
+        when(repository.getEventsBy("xxxx")).thenReturn(storedEvents());
+        useCase.addRepository(repository);
+
+        //act
+
+        var events = UseCaseHandler.getInstance()
+                .setIdentifyExecutor(command.getMentoriaId().value())
+                .syncExecutor(useCase, new RequestCommand<>(command))
+                .orElseThrow()
+                .getDomainEvents();
+
+        //assert
+        var event = (DirectrizAgregadaAMentoria)events.get(0);
+        Assertions.assertEquals("Curso DDD", event.getDirectiz().value());
+    }
+
+    private List<DomainEvent> storedEvents() {
+        MentoriaId mentoriaId = new MentoriaId("xxxx");
+        Nombre nombre = new Nombre("Aprendiendo de casos de usos");
+        Fecha fecha = new Fecha(LocalDateTime.now(), LocalDate.now());
+
+        var event = new MentoriaCreada(
+                mentoriaId,
+                nombre,
+                fecha
+        );
+
+        return List.of(event);
+    }
+}

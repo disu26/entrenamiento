bdfae09cf9a7b35142225bc1b16ebe676b7ae6d6
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-05-16 15:41:14 -0500

    AgregarDirectrizMentoria Command y UseCase. Se crea el comando y se implementa en el caso de uso de AgregarDirectrizMentoria.

---
+++ business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizMentoriaUseCase.java
@@ -0,0 +1,13 @@
+package co.com.sofkau.entrenamento.curso;
+
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizMentoria;
+
+public final class AgregarDirectrizMentoriaUseCase extends UseCase<RequestCommand<AgregarDirectrizMentoria>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<AgregarDirectrizMentoria> agregarDirectrizMentoriaRequestCommand) {
+
+    }
+}
